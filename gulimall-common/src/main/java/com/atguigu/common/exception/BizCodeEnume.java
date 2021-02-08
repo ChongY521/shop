4c0dfe83c704e2c9f4fd3ee53f018f764653d487
chongpeng <1062568521@qq.com>
2021-02-08 10:22:30 +0800

    测试枚举

--- gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
+++ gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
@@ -16,12 +16,13 @@ package com.atguigu.common.exception;
  *
  */
 public enum BizCodeEnume {
-    UNKNOW_EXCEPTION(10000,"系统未知异常"),
-    VAILD_EXCEPTION(10001,"参数格式校验失败");
+    UNKNOW_EXCEPTION(10000, "系统未知异常"),
+    VAILD_EXCEPTION(10001, "参数格式校验失败");
 
     private int code;
     private String msg;
-    BizCodeEnume(int code,String msg){
+
+    BizCodeEnume(int code, String msg) {
         this.code = code;
         this.msg = msg;
     }
@@ -32,5 +33,15 @@ public enum BizCodeEnume {
 
     public String getMsg() {
         return msg;
+    }
+
+
+    public static BizCodeEnume getByCode(int code) {
+        for (BizCodeEnume value : BizCodeEnume.values()) {
+            if (value.getCode() == code) {
+                return value;
+            }
+        }
+        return null;
     }
 }
